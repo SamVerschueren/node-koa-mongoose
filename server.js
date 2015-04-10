e0284c2775903c1c32277e1adbec572ea9f36719
Sam Verschueren <sam.verschueren@gmail.com>
2015-04-10 15:29:17 +0200

    Used 4 spaces instead of a tab in the server.js file and added an author and since tag.

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 'use strict';
 
+/**
+ * The main entry file of the application.
+ * 
+ * @author Sam Verschueren      <sam.verschueren@gmail.com>
+ * @since  9 April 2015
+ */
+
 // module dependencies
 var koa = require('koa'),
-	router = require('koa-router')();
+    router = require('koa-router')();
 
 var config = require('./config/config')[process.env.NODE_ENV || 'development'];
-
 var app = koa();
 
 // Configure routes
@@ -16,5 +22,5 @@ require('./config/koa')(app, router, config);
 
 var port = process.argv[2] || config.app.port || 1337;
 app.listen(port, function() {
-	console.log('Server started on port ' + port);
+    console.log('Server started on port ' + port);
 });
